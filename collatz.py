7338febce0bb6442c5712dd2fbdef73c6da450de
S May <s.seanmay@gmail.com>
2020-05-02 22:19:36 -0500

    Initial Commit

---
+++ collatz.py
@@ -0,0 +1,20 @@
+def collatz(number):
+	if number % 2 ==0:
+		print(number // 2)
+		return number//2
+	elif number % 2 ==1:
+		print(3* number + 1)
+		return 3*number+1	
+
+ask=int(input("Give me a number "))
+while ask != 1:
+	ask = collatz(ask)
+	
+
+
+
+
+
+
+
+
